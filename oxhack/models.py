bb3406e8aa361675a8ebd1515c714465459f4c77
Kacper Zylka <kacper.zylka12@imperial.ac.uk>
2014-11-29 15:33:05 +0000

    Added our models to admin

--- oxhack/models.py
+++ oxhack/models.py
@@ -2,28 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class UserProfile(models.Model):
-    # One-to-one mapping with a django.contrib.auth.models.User object
-    user = models.OneToOneField(User)
-    # Many-to-one mapping with a College object
-    college = models.ForeignKey(College)
-    # Many-to-many mapping with Challenge objects
-    challenges = models.ManyToManyField(Challenge)
-
-
-class College(models.Model):
-    name = models.CharField(max_length=200, choices=COLLEGES)
-    latitude = models.FloatField('Latitude', blank=True, null=True)
-    longitude = models.FloatField('Longitude', blank=True, null=True)
-
-
-class Challenge(models.Model):
-    text = models.CharField(max_length=1000)
-    answer = models.CharField(max_length=200)  # TODO foreign key in an Answer model?
-    difficulty = models.IntegerField(choices=DIFFICULTIES)
-    college = models.ForeignKey(College)
-
-
 # List of college names and codes
 COLLEGES = (
     ('MANSFIELD', 'MAN'),
@@ -40,3 +18,26 @@ DIFFICULTIES = (
     (MODERATE, 'Moderate'),
     (HARD, 'Hard'),
 )
+
+
+class College(models.Model):
+    name = models.CharField(max_length=200, choices=COLLEGES)
+    latitude = models.FloatField('Latitude', blank=True, null=True)
+    longitude = models.FloatField('Longitude', blank=True, null=True)
+
+
+class Challenge(models.Model):
+    text = models.CharField(max_length=1000)
+    answer = models.CharField(max_length=200)  # TODO foreign key in an Answer model?
+    difficulty = models.IntegerField(choices=DIFFICULTIES)
+    college = models.ForeignKey(College)
+
+
+class UserProfile(models.Model):
+    # One-to-one mapping with a django.contrib.auth.models.User object
+    user = models.OneToOneField(User)
+    # Many-to-one mapping with a College object
+    college = models.ForeignKey(College)
+    # Many-to-many mapping with Challenge objects
+    challenges = models.ManyToManyField(Challenge)
+
