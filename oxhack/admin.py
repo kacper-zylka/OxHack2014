d78fcf71d52625c07b8b46a58157b317d6ae9424
Kacper Zylka <kacper.zylka12@imperial.ac.uk>
2014-11-29 15:33:05 +0000

    Added our models to admin

---
+++ oxhack/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from models import College, Challenge, UserProfile
+
+admin.site.register(College)
+admin.site.register(Challenge)
+admin.site.register(UserProfile)
