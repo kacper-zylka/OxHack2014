a55101f0b53ddb42ca9316e843c30612ab839f0b
Kacper Zylka <kacper.zylka12@imperial.ac.uk>
2014-11-30 02:46:14 +0000

    Added secret file for storing credentials

--- oxhack/settings.py
+++ oxhack/settings.py
@@ -105,27 +105,20 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd8jberfnlbn2mv',
-        'USER': 'ptielcjqzkcixq',
-        'PASSWORD': 'r9XelJoJxcFOxqbL99h-opYkP_',
-        'HOST': 'ec2-107-20-197-146.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+import secret
+
+DATABASES = secret.DATABASES
 
 # User registration settings for django-registration-redux
 ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window
 REGISTRATION_AUTO_LOGIN = True # Automatically log the user in.
 
-# Temporary email settings for registration
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'frigaardj'
-EMAIL_HOST_PASSWORD = 'oxhunt2014'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+
+EMAIL_HOST = secret.EMAIL_HOST
+EMAIL_HOST_USER = secret.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = secret.EMAIL_HOST_PASSWORD
+EMAIL_PORT = secret.EMAIL_PORT
+EMAIL_USE_TLS = secret.EMAIL_USE_TLS
 
 TEMPLATE_CONTEXT_PROCESSORS = (
 
