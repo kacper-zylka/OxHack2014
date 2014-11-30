13d077e4a8336f34b22dd76ae393731a863c30ac
frigaardj <jack.frigaard@gmail.com>
2014-11-30 01:00:19 +0000

    email interface progress

--- oxhack/models.py
+++ oxhack/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # List of college names and codes
 COLLEGES = (
     ('MAN', 'Mansfield'),
--- oxhack/views.py
+++ oxhack/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 
-from oxhack.models import College, Challenge, ChallengeCompletion, DIFFICULTIES
+from oxhack.models import College, Challenge, ChallengeCompletion, UserProfile, DIFFICULTIES
+from django.contrib.auth.models import User
+
+from django.utils import timezone
 
 import sendgrid
 from django.shortcuts import render_to_response
@@ -18,20 +21,46 @@ def inbound(request):
     from_address = request.POST.get('from','')
     subject = request.POST.get('subject','')
     body = request.POST.get('body','')
+    userQuery = User.objects.filter(email=from_address).exclude(username='admin')
+
+    # No account with that email exists
+    if len(userQuery) != 1:
+        sendUserNotEnrolledEmail(to_address=from_address)
+        return HttpResponse("Email address not enrolled")
+
+    userProfile = UserProfile.objects.get(user=userQuery[0])
 
     # If it has a blank subject, send them instructions on how to play
     if not subject:
         sendBlankSubjectEmail(to_address=from_address)
-        return HttpResponse("Blank subject")
+        return HttpResponse("Email contained blank subject")
     # If it is a reply to a clue list email, parse it for answers
     elif reply_prefix in subject:
+        debug_response = ''
         # Trim off the prefix to just get the college name
         college_name = subject[len(reply_prefix):]
         college = College.objects.get(name__icontains=college_name)
         # TODO get the user from their email
-        incompleted_challenges = incompletedChallengesForUserCollege(college=college)
+        incompleted_challenges = incompletedChallengesForUserCollege(college=college, userProfile=userProfile)
+        debug_response += 'incompleted_challenges: \n' + str(incompleted_challenges)
+        answers = parseAnswerStringForAnswers(body)
+        debug_response += '\n correct answers:\n'
 
-        return HttpResponse("Received answers")
+        reply_email
+
+        for ans_key, ans_value in answers.iteritems():
+            # they might have since completed the challenge they are emailing about
+            try:
+                chall = incompleted_challenges.index(ans_key - 1)
+                if chall != None and chall.answer == ans_value:
+                    # make them complete the challenge
+                    c = ChallengeCompletion(user=userProfile, challenge=incompleted_challenges[ans_key - 1], time=timezone.now())
+                    c.save()
+                    debug_response += str(c)
+            except ValueError:
+                pass
+
+        return HttpResponse(debug_response)
 
     # Else they are probably asking for a college clue list. Check
     else:
@@ -44,41 +73,36 @@ def inbound(request):
             return HttpResponse("Could not match college subject.")
 
         # If the body is empty, send them the clue list for that college
-        if not body:
-            sendClueListing(to_address=from_address, college=matching_colleges[0])
-            return HttpResponse("Sent clues list for college.")
-
-
-        # sg = sendgrid.SendGridClient('frigaardj', 'oxhunt2014')
-
-        # message = sendgrid.Mail(to=from_addr, subject="email to: " + from_addr, html=body, text=body, from_email='hunt@oxhunt.me')
-        # status, msg = sg.send(message)
+        if not body.strip():
+            text = getClueList(college=matching_colleges[0], userProfile=userProfile)
+            sendEmail(from_address,reply_prefix + matching_colleges[0].name,text)
+            return HttpResponse(text)
 
     return HttpResponse("Pass")
 
 
-def incompletedChallengesForUserCollege(college, user):
+def incompletedChallengesForUserCollege(college, userProfile):
     all_challenges = Challenge.objects.filter(college=college)
-    completed_challenges = [challComp.challenge for challComp in ChallengeCompletion.objects.all()]
+    completed_challenges = [challComp.challenge for challComp in ChallengeCompletion.objects.filter(user=userProfile)]
     return [chall for chall in all_challenges if chall not in completed_challenges]
 
+def sendUserNotEnrolledEmail(to_address):
+    subject = "Woops - unrecognised email address!"
+    body = "We didn't recognise your email address. If you haven't created an account, click here."
+    return sendEmail(to_address = to_address , subject = subject, text = body)
 
-def sendClueListing(to_address, college):
-    incompleted_challenges = incompletedChallengesForUserCollege(college=college)
+
+def getClueList(college, userProfile):
+    incompleted_challenges = incompletedChallengesForUserCollege(college=college, userProfile=userProfile)
 
     clues_list = []
     for i, chall in enumerate(incompleted_challenges):
         clues_list.append( str(i+1) + ": (" + dict(DIFFICULTIES)[chall.difficulty] + ") " + chall.text)
 
-    # clues_list = [ str(idx + 1) + ": " + DIFFICULTIES[chall.difficulty] + " " + chall.text for idx, chall in enumerate(incompleted_challenges)]
-
     body = "Here are the available clues for " + college.name + ":\n\n"
     body += '\n'.join(clues_list)
     body += "\n\nReply to this message with the following format to answer:\n\n1: Answer to question 1 \n2: Answer to question 2\netc."
-
-    subject = reply_prefix + college.name
-    return sendEmail(to_address=to_address,subject=subject,text=body)
-
+    return body
 
 def sendBlankSubjectEmail(to_address):
     subject = "Woops - blank subject!"
@@ -108,7 +132,6 @@ def parseAnswerStringForAnswers(answer_string):
     answers = {}
 
     while len(ans) > 0:
-        print('loopstart')
         first_colon = ans.find(':')
         i = ans[first_colon - 1]
         ans = ans[first_colon + 1 :]
@@ -117,7 +140,6 @@ def parseAnswerStringForAnswers(answer_string):
             next_colon = len(ans) + 1
         answers[int(i)] = ans[: next_colon - 1 ]
         ans = ans[ next_colon - 1 : ]
-        print(ans)
     return answers
 
 def home(request):
